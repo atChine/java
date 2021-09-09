c46045fa7a1c2892927d17c976d7fd98fcb9504d
GaoHaiNB <88870728+GaoHaiNB@users.noreply.github.com>
2021-09-09 22:38:05 +0800

    Add files via upload

---
+++ SeptemberNine.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SeptemberNine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		String s;
+		s=in.nextLine();
+		System.out.println(s);
+		System.out.println(s=="bay");
+		
+	}
+
+}
