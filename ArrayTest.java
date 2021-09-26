26fa95e56594bf290960bedadfcec192501c755c
GaoHaiNB <88870728+GaoHaiNB@users.noreply.github.com>
2021-09-26 20:12:03 +0800

    Add files via upload

---
+++ ArrayTest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ArrayTest {
+    public static void main(   String[] args) {
+        System.out.printf(" ");
+        Scanner scanner = new Scanner(system.in);
+        for (int i = 0; i < 5;  i++) {
+            System.out.printf("");
+        }
+        
+    }
+}
