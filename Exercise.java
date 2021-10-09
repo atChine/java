4da20fd2977dda1e6ffb8af327da9524e53036fc
GaoHaiNB <88870728+GaoHaiNB@users.noreply.github.com>
2021-10-09 22:31:02 +0800

    Add files via upload

--- Exercise.java
+++ Exercise.java
@@ -1,35 +1,18 @@
-
 public class Exercise {
+    public static void main(String[] args) {
+        int n = 1;
+        Integer integer = new Integer(n);
+        System.out.println(integer);
 
-	public static void main(String[] args) {
-		A01 a01=new A01();
-		double arr[]= null;//{};
-		Double res=a01.max(arr);
-		if(res!=null)
-		{
-		System.out.println("arr的最大值为="+a01.max(arr));
-		}
-		else {
-			System.out.println("输入的arr值有误");
-		}
-	}
+        Integer integer1 = new Integer(50);
+        int i1 = integer1.intValue();
 
-}
+        int a = 10;
+        String a1 = Integer.toString(a);
+        System.out.println(a1);
 
-class A01{
-	public Double max(double[] arr) {
-		if(arr!=null && arr.length>0) {
-			double max=arr[0];
-			for(int i=1;i<arr.length;i++)
-			{
-				if(max<arr[i])
-				{
-					max=arr[i];
-				}
-			}
-			return max;
-			}
-		else
-		return null;
-	}
+        String b = "123";
+        int b1 = Integer.parseInt(b);
+        System.out.println(b1);
+    }
 }
