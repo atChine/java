024a2aad766b16facb32133ed1995b5f3f4d1e4e
GaoHaiNB <88870728+GaoHaiNB@users.noreply.github.com>
2021-11-09 21:52:16 +0800

    Add files via upload

---
+++ 123.sql
@@ -0,0 +1,13 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.7.19 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `user` (
+	`id` int ,
+	`name` varchar ,
+	`password` varchar 
+); 
+insert into `user` (`id`, `name`, `password`) values('1','jack','123');
